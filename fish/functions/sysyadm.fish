9bfa2accd4ff2c02953216624e4f56a132f8fe45
blue-witcher <171081467+blue-witcher@users.noreply.github.com>
2024-06-03 00:29:54 +0200

    Delete fish/functions/sysyadm.fish

--- fish/functions/sysyadm.fish
+++
@@ -1,4 +0,0 @@
-function sysyadm --wraps='sudo yadm --yadm-dir /etc/yadm --yadm-data /etc/yadm/data' --description 'alias sysyadm=sudo yadm --yadm-dir /etc/yadm --yadm-data /etc/yadm/data'
-  sudo yadm --yadm-dir /etc/yadm --yadm-data /etc/yadm/data $argv
-        
-end
