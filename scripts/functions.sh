328ad2ddf5c4774a3c8c392cdc1c42399ffb3a82
blueOnWindows <171081467+blue-witcher@users.noreply.github.com>
2024-06-07 04:06:17 +0200

    Moved functions to scripts/functions.sh Deleted functions from bootstrap

---
+++ scripts/functions.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+## Function to determine the Linux distribution
+detect_distro() {
+    if [ -f /etc/os-release ]; then
+        # Read the distribution information from /etc/os-release
+        . /etc/os-release
+        DISTRO=$NAME
+        DISTRO_VERSION=$VERSION_ID
+    elif [ -f /etc/lsb-release ]; then
+        # Read the distribution information from /etc/lsb-release
+        . /etc/lsb-release
+        DISTRO=$DISTRIB_ID
+        DISTRO_VERSION=$DISTRIB_RELEASE
+    elif [ -f /etc/debian_version ]; then
+        DISTRO="Debian"
+        DISTRO_VERSION=$(cat /etc/debian_version)
+    elif [ -f /etc/redhat-release ]; then
+        DISTRO=$(cat /etc/redhat-release)
+        DISTRO_VERSION=$(cat /etc/redhat-release | grep -oP '\d+(\.\d+)?')
+    else
+        DISTRO="Unknown"
+        DISTRO_VERSION="Unknown"
+    fi
+    echo "Detected Linux distribution: $DISTRO"
+    echo "Detected distro version: $DISTRO_VERSION"
+}
+
+## Function to ask the user a yes/no question
+ask_user() {
+    while true; do
+        read -p "$1 (Y/n): " yn
+        yn=${yn:-y}
+        case $yn in
+            [Yy] ) return 0;;
+            [Nn] ) return 1;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+}
+
+## Function to get the current user
+get_current_user() {
+    CURRENT_USER=$(whoami)
+    echo "Current user: $CURRENT_USER"
+}
