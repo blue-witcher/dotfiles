3856c913008bef33de02d9d4e14b0d1dfe14d374
blueDesktop <171081467+blue-witcher@users.noreply.github.com>
2024-08-30 23:31:38 +0200

    Changed driveBiSync to be run from ~/dotfiles

--- rclone/driveBiSync.sh
+++ rclone/driveBiSync.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/bin/rclone bisync 'obsidianVaults:' '/home/blue/Documents/Obsidian Vaults/' --create-empty-src-dirs --compare size,modtime --check-access  --recover --remove-empty-dirs --verbose --log-file='/home/blue/Documents/scripts/driveSync/logs/driveBiSync.log' --conflict-resolve newer --conflict-suffix 'conflict.md' --delete-after
+/usr/bin/rclone bisync 'obsidianVaults:' '/home/blue/Documents/Obsidian Vaults/' --create-empty-src-dirs --compare size,modtime --check-access  --recover --remove-empty-dirs --verbose --log-file='/home/blue/dotfiles/rclone/driveBiSync.log' --conflict-resolve newer --conflict-suffix 'conflict.md' --delete-after
