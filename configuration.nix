7e807f95e0748ff3633469a44929bddcac2d6f95
blue-witcher <blue-witcher>
2024-09-16 14:26:54 +0200

    updated nix conf.

--- configuration.nix
+++ configuration.nix
@@ -81,7 +81,7 @@
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  services.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.blue = {
@@ -89,11 +89,7 @@
     description = "blue";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
-      kdePackages.kate
-      fastfetch
-      fish
       discord
-      git
     #  thunderbird
     ];
   };
@@ -126,6 +122,11 @@
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
+    micro
+    fish
+    git
+    fastfetch
+    kdePackages.kate
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
