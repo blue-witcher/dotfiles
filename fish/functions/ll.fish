904df42a0091168012d4873641e4e0b1806461d9
blue-witcher <171081467+blue-witcher@users.noreply.github.com>
2024-09-22 14:37:00 +0200

    directories now always get listed at the top

--- fish/functions/ll.fish
+++ fish/functions/ll.fish
@@ -1,6 +1,7 @@
-#
+# original function is from /usr/share/fish/functions/ll.fish
+# the only change I made was to add the -a flag to display all files, even hidden ones.
 # These are very common and useful
 #
 function ll --wraps ls --description "List contents of directory using long format"
-    ls -lha --group-directories-first $argv
+    ls -lha $argv
 end
