485ead1f5e8ac24e426ea0f39551aa7edf6898c8
blue-witcher <>
2024-09-22 19:09:03 +0200

    deleted micro colorschemes and added auto-downloader script

---
+++ micro/colorschemes/downloadColorschemes.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget -P ~/.config/micro/colorschemes/ https://raw.githubusercontent.com/catppuccin/micro/refs/heads/main/src/catppuccin-mocha-transparent.micro
+wget -P ~/.config/micro/colorschemes/ https://raw.githubusercontent.com/catppuccin/micro/refs/heads/main/src/catppuccin-mocha.micro
