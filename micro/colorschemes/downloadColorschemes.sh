a7b794548cb4db7b6dcdd95431965f62821d5b61
blue-witcher <>
2024-09-22 19:19:12 +0200

    added user feedback to script

--- micro/colorschemes/downloadColorschemes.sh
+++ micro/colorschemes/downloadColorschemes.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+echo "Installing catppuccin-mocha-transparent micro colorscheme..."
 wget -P ~/.config/micro/colorschemes/ https://raw.githubusercontent.com/catppuccin/micro/refs/heads/main/src/catppuccin-mocha-transparent.micro
+echo "Installed catppuccin-mocha-transparent micro colorscheme."
+
+echo "Installing catppuccin-mocha micro colorscheme..."
 wget -P ~/.config/micro/colorschemes/ https://raw.githubusercontent.com/catppuccin/micro/refs/heads/main/src/catppuccin-mocha.micro
+echo "Installed catppuccin-mocha micro colorscheme."
