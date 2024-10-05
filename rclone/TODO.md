d185788013190d0dc01db2c16209c43d42355b15
blue-witcher <>
2024-10-06 00:35:38 +0200

    achieved everything outlined in TODO.md file

--- rclone/TODO.md
+++
@@ -1,6 +0,0 @@
-# things that need to be done
-
-- enable cronie service in script
-- create .var/log/logfilename.log directory and file in script
-- run command with --resync flag in script
-- run rclone config in script
--- rclone/install_rclone_config.sh
+++ rclone/install_rclone_config.sh
@@ -8,3 +8,21 @@ mv ~/.config/rclone/rclone.conf ~/.config/rclone/rclone.conf.backup
 
 # copy rclone.conf to folder
 cp ~/dotfiles/rclone/rclone.conf ~/.config/rclone/
+
+# tell the user to configure their token
+echo "Please complete your rclone remote setup (you just need to authorize with your google account)."
+rclone config
+
+# create driveBiSync log file
+mkdir -p ~/.var/log/
+touch ~/.var/log/driveBiSync.log
+
+# create RCLONE_TEST file
+mkdir -p ~/Documents/Obsidian\ Vaults/
+touch ~/Documents/Obsidian\ Vaults/RCLONE_TEST
+
+# enable cronie service
+sudo systemctl enable --now cronie
+
+# run first run with --resync flag
+/usr/bin/rclone bisync 'obsidianVaults:' "$HOME/Documents/Obsidian Vaults/" --create-empty-src-dirs --compare size,modtime --check-access  --recover --remove-empty-dirs --verbose --log-file="$HOME/.var/log/driveBiSync.log" --conflict-resolve newer --conflict-suffix 'conflict.md' --delete-after --resync
