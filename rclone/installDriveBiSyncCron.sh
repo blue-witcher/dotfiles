ff5c9f9a905b4c4f99cba9c39a98957fb54e8784
blue-witcher <>
2024-10-06 00:38:43 +0200

    moved enablement of cronie service to correct script

--- rclone/installDriveBiSyncCron.sh
+++ rclone/installDriveBiSyncCron.sh
@@ -5,3 +5,6 @@ DRIVESYNC_CRON=$(< ~/dotfiles/rclone/driveBiSync.cron)
 echo "Installing driveBiSync cron jobs.."
 (crontab -l; echo "$DRIVESYNC_CRON") | crontab
 echo "Installed driveBiSync cron jobs."
+
+# enable cronie service
+sudo systemctl enable --now cronie
--- rclone/install_rclone_config.sh
+++ rclone/install_rclone_config.sh
@@ -21,8 +21,5 @@ touch ~/.var/log/driveBiSync.log
 mkdir -p ~/Documents/Obsidian\ Vaults/
 touch ~/Documents/Obsidian\ Vaults/RCLONE_TEST
 
-# enable cronie service
-sudo systemctl enable --now cronie
-
 # run first run with --resync flag
 /usr/bin/rclone bisync 'obsidianVaults:' "$HOME/Documents/Obsidian Vaults/" --create-empty-src-dirs --compare size,modtime --check-access  --recover --remove-empty-dirs --verbose --log-file="$HOME/.var/log/driveBiSync.log" --conflict-resolve newer --conflict-suffix 'conflict.md' --delete-after --resync
