824fa1c93e3e14f9d7ec458751833162e1a57407
blue-witcher <>
2024-10-06 00:34:50 +0200

    FINALLY MADE IT USER INDEPENDENT (yes I forgot $HOME exists, pls don't bully

--- rclone/driveBiSync.sh
+++ rclone/driveBiSync.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/bin/rclone bisync 'obsidianVaults:' '/home/blue/Documents/Obsidian Vaults/' --create-empty-src-dirs --compare size,modtime --check-access  --recover --remove-empty-dirs --verbose --log-file='/home/blue/.var/log/driveBiSync.log' --conflict-resolve newer --conflict-suffix 'conflict.md' --delete-after
+/usr/bin/rclone bisync 'obsidianVaults:' "$HOME/Documents/Obsidian Vaults/" --create-empty-src-dirs --compare size,modtime --check-access  --recover --remove-empty-dirs --verbose --log-file="$HOME/.var/log/driveBiSync.log" --conflict-resolve newer --conflict-suffix 'conflict.md' --delete-after
