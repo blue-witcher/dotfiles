ecab5bec4d31b4e4cb1ade9db245b526b8cdd802
Blue-Witcher <>
2024-10-08 21:51:02 +0200

    reorganized konsole dotfiles into own folder

--- KDE/konsole/install_konsole_config.sh
+++ KDE/konsole/install_konsole_config.sh
@@ -2,11 +2,11 @@
 
 # make directories if not already exist
 mkdir -p ~/.config
-mkdir -p ~/.local/share/konsole/
+mkdir -p ~/.local/share/konsole
 
 # backup previous konsolerc
 mv ~/.config/konsolerc ~/.config/konsolerc.backup
 
 # symlink files
-ln -s ~/dotfiles/KDE/konsolerc ~/.config/
-ln -s ~/dotfiles/KDE/.local/share/konsole/* ~/.local/share/konsole
+ln -s ~/dotfiles/KDE/konsole/konsolerc ~/.config/
+ln -s ~/dotfiles/KDE/konsole/colorschemesAndProfiles/* ~/.local/share/konsole/
