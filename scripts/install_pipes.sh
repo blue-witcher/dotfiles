502c49eb0eb9915b010a36bc4ba038328fa1fc2c
blue-witcher <>
2024-10-18 01:34:15 +0200

    added pipes.sh install script

---
+++ scripts/install_pipes.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Downloading pipes.sh version 1.3.0"
+echo "sudo wget -P '/usr/local/bin/' 'https://raw.githubusercontent.com/pipeseroni/pipes.sh/65481cb6b6f8bebc11077192c111a419b70625aa/pipes.sh'"
+sudo wget -P '/usr/local/bin/' 'https://raw.githubusercontent.com/pipeseroni/pipes.sh/65481cb6b6f8bebc11077192c111a419b70625aa/pipes.sh' && sudo chmod +x '/usr/local/bin/pipes.sh'
+
+echo "Downloading pipes.sh manpage(6) v1.3.0"
+echo "sudo wget -P '/usr/local/share/man/man6/' 'https://raw.githubusercontent.com/pipeseroni/pipes.sh/65481cb6b6f8bebc11077192c111a419b70625aa/pipes.sh.6'"
+sudo wget -P '/usr/local/share/man/man6/' 'https://raw.githubusercontent.com/pipeseroni/pipes.sh/65481cb6b6f8bebc11077192c111a419b70625aa/pipes.sh.6'
