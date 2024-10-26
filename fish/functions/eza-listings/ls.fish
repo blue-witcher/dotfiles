0fd1ae959c0436cb677eb3494bbabaee836f04fb
blue-witcher <>
2024-10-26 15:50:05 +0200

    added info to function description

--- fish/functions/eza-listings/ls.fish
+++ fish/functions/eza-listings/ls.fish
@@ -1,3 +1,3 @@
-function ls --wraps='eza --color=always --group-directories-first --icons --smart-group --git' --description 'alias ls=eza --color=always --group-directories-first --icons --smart-group --git'
+function ls --wraps='eza --color=always --group-directories-first --icons --smart-group --git --links' --description 'alias ls=eza --color=always --group-directories-first --icons --smart-group --git --links'
     eza --color=always --group-directories-first --icons --smart-group --git --links $argv
 end
