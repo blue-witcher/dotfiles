d0c616390cecdec149d4ce212be4c8de01d476fb
blue-witcher <>
2024-10-26 15:44:32 +0200

    added hard link counter to all eza commands

--- fish/functions/eza-listings/ls.fish
+++ fish/functions/eza-listings/ls.fish
@@ -1,3 +1,3 @@
 function ls --wraps='eza --color=always --group-directories-first --icons --smart-group --git' --description 'alias ls=eza --color=always --group-directories-first --icons --smart-group --git'
-    eza --color=always --group-directories-first --icons --smart-group --git $argv
+    eza --color=always --group-directories-first --icons --smart-group --git --links $argv
 end
