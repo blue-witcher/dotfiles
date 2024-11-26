f145de31eb6efd056a7938571998d23219849983
blue-witcher <>
2024-11-26 17:55:17 +0100

    changed auto ls to have a toggle for auto ls all

--- fish/conf.d/autols.fish
+++ fish/conf.d/autols.fish
@@ -1,10 +1,15 @@
 function __autols_hook --description "Auto ls including hidden files" --on-event fish_prompt
-  if test "$NO_AUTO_LS" != ""
+  if test "$NO_AUTO_LS" = 1
     return
   end
 
   if test "$__autols_last" != (pwd)
-    echo; ls -a
+    if test "$AUTO_LS_ALL" = 1
+      echo; ls -a
+    else
+      echo; ls
+    end
+    set -g __autols_last (pwd)
   end
-  set  -g __autols_last (pwd)
+
 end
