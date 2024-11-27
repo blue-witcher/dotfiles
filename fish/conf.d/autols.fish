1abcd97b7f5bc6061a2fa5868c9fa7db13ed8d4d
blue-witcher <>
2024-11-28 00:37:30 +0100

    made easier to read and no more nested if-statement I think nested if-statements are bad? Should look more into it though.

--- fish/conf.d/autols.fish
+++ fish/conf.d/autols.fish
@@ -1,15 +1,18 @@
 function __autols_hook --description "Auto ls including hidden files" --on-event fish_prompt
-  if test "$NO_AUTO_LS" = 1
+  if test "$NO_AUTO_LS" = 1             # Test if auto_ls is turned off
     return
   end
 
-  if test "$__autols_last" != (pwd)
-    if test "$AUTO_LS_ALL" = 1
-      echo; ls -a
-    else
-      echo; ls
-    end
-    set -g __autols_last (pwd)
+  if test "$__autols_last" = (pwd)      # Test if directory changed
+    return
   end
+
+  if test "$AUTO_LS_ALL" = 1            # test if auto_ls -all flag is set
+    echo; ls -a
+  else
+    echo; ls
+  end
+
+  set -g __autols_last (pwd)            # set new directory in variable
 
 end
