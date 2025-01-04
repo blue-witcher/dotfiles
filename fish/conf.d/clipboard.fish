4623d34da248169b5bc46101035796b9bf464333
blue-witcher <>
2025-01-04 11:45:56 +0100

    change pst to wl-paste in all cases change to wl-copy in non-kitty case

--- fish/conf.d/clipboard.fish
+++ fish/conf.d/clipboard.fish
@@ -1,9 +1,8 @@
 if status is-interactive
     if test $TERM = 'xterm-kitty'
         abbr -a cb 'kitten clipboard'
-        abbr -a pst 'fish_clipboard_paste'
     else
-        abbr -a cb 'fish_clipboard_copy'
-        abbr -a pst 'fish_clipboard_paste'
+        abbr -a cb 'wl-copy'
     end
+    abbr -a pst 'wl-paste'
 end
