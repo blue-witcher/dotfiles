b446f72bc9c3ce1d3b0144f043627b3d492bb56c
blue-witcher <>
2025-01-04 07:22:58 +0100

    changed kitten pst abbr to fish_clipboard_paste to avoid the "do you wanna access clipboard contents?" prompt

--- fish/conf.d/clipboard.fish
+++ fish/conf.d/clipboard.fish
@@ -1,6 +1,7 @@
 if status is-interactive
     if test $TERM = 'xterm-kitty'
         abbr -a cb 'kitten clipboard'
+        abbr -a pst 'fish_clipboard_paste'
     else
         abbr -a cb 'fish_clipboard_copy'
         abbr -a pst 'fish_clipboard_paste'
