13056bcb2145edfd1d5c9c732f7623d136f27b8e
Blue-Witcher <>
2025-01-06 23:40:43 +0100

    add --hyperlinks option

--- fish/functions/eza-listings/ls.fish
+++ fish/functions/eza-listings/ls.fish
@@ -1,3 +1,3 @@
-function ls --wraps='eza --color=always --group-directories-first --icons --smart-group --git --links --no-quotes' --description 'alias ls=eza --color=always --group-directories-first --icons --smart-group --git --links --no-quotes'
-    eza --color=always --group-directories-first --icons --smart-group --git --links --no-quotes $argv
+function ls --wraps='eza --color=always --group-directories-first --icons --smart-group --git --links --no-quotes --hyperlinks' --description 'alias ls=eza --color=always --group-directories-first --icons --smart-group --git --links --no-quotes --hyperlinks'
+    eza --color=always --group-directories-first --icons --smart-group --git --links --no-quotes --hyperlinks $argv
 end
