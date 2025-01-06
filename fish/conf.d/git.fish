eb34b5ec41e696f1a60a0dec82d87fcdbfd40e8e
blue-witcher <>
2025-01-06 07:11:49 +0100

    add range from HEAD to origin/HEAD

--- fish/conf.d/git.fish
+++ fish/conf.d/git.fish
@@ -2,5 +2,5 @@ if status is-interactive
     abbr -a gits 'git status'
     abbr -a gitf 'git fetch'
     abbr -a gitfs 'git fetch ; git status'
-    abbr -a gitl 'git log'
+    abbr -a gitl 'git log HEAD origin/HEAD'
 end
