c8642331900d9a564c212343ccf0d17f2163ce78
blue-witcher <>
2025-01-08 07:00:28 +0100

    add get_script_dir function

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -44,3 +44,9 @@ get_current_user() {
     CURRENT_USER=$(whoami)
     echo "Current user: $CURRENT_USER"
 }
+
+## Function to find the place the script is located in
+get_script_dir() {
+	__SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+	echo "$__SCRIPT_DIR"
+}
