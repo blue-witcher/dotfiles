9590aa8eced7bb1a6f1248fac02736815918331e
blue-witcher <>
2025-01-17 15:32:09 +0100

    Revert "add sort by extension"
    This reverts commit 0b64dc820231b21eb7b727cf6ee10588ce523d33.

--- fish/functions/eza-listings/ls.fish
+++ fish/functions/eza-listings/ls.fish
@@ -1,3 +1,3 @@
-function ls --wraps='eza --color=always --group-directories-first --icons --smart-group --git --links --no-quotes --hyperlink --sort extension' --description 'alias ls=eza --color=always --group-directories-first --icons --smart-group --git --links --no-quotes --hyperlink --sort extension'
-    eza --color=always --group-directories-first --icons --smart-group --git --links --no-quotes --hyperlink --sort extension $argv
+function ls --wraps='eza --color=always --group-directories-first --icons --smart-group --git --links --no-quotes --hyperlink' --description 'alias ls=eza --color=always --group-directories-first --icons --smart-group --git --links --no-quotes --hyperlink'
+    eza --color=always --group-directories-first --icons --smart-group --git --links --no-quotes --hyperlink $argv
 end
