f5165d0575a3239528116d82346f9cc742a5df44
blue-witcher <>
2025-01-19 06:36:28 +0100

    added feedback when function not applicable

--- fish/functions/package_management/yay.fish
+++ fish/functions/package_management/yay.fish
@@ -1,11 +1,16 @@
 if type -q paru
     function yay --wraps=paru --description 'alias yay=paru'
       paru $argv
-            
     end
+
 else if type -q zypper
     function yay --wraps=zypper --description 'alias yay=zypper'
       zypper $argv
-            
     end
+
+else
+    function yay
+        echo 'Function not applicable.'
+    end
+
 end
--- fish/functions/package_management/yayy.fish
+++ fish/functions/package_management/yayy.fish
@@ -1,6 +1,11 @@
 if type -q zypper
     function yayy --wraps='zypper' --description 'alias yayy=sudo zypper'
       sudo zypper $argv
-            
     end
+
+else
+    function yayy
+        echo 'Function not applicable.'
+    end
+
 end
