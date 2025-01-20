e9b528136207d3a0805e84c8c1ebe4d04ce0225c
blue-witcher <>
2025-01-20 22:02:28 +0100

    Revert "add --total-size argument"
    takes ages to execute when doing it on home directory for example (sadge, du -hs works way faster and I wonder why. Hopefully that eza is as fast someday)
    This reverts commit 94962c7556683c38cf60ac689507aae703f2b17e.

--- fish/conf.d/listings.fish
+++ fish/conf.d/listings.fish
@@ -2,7 +2,7 @@ if status is-interactive
     if type -q eza
         abbr -a lsa 'ls -a'
         abbr -a lt 'ls --tree'
-        abbr -a llt 'ls --total-size -Tl'
+        abbr -a llt 'ls -Tl'
         abbr -a lta 'ls -Ta'
         abbr -a llta 'll --tree'
     end
