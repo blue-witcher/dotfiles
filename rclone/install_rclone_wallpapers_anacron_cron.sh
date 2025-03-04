a90f04e3d286818850d8bb1372e796ecdd30e25f
Blue-Witcher <>
2025-03-04 17:05:14 +0100

    fixed anacrontab by making the command a symlink to the script instead to avoid having to store the anacrontab in a variable

--- rclone/install_rclone_wallpapers_anacron_cron.sh
+++ rclone/install_rclone_wallpapers_anacron_cron.sh
@@ -11,6 +11,10 @@ echo "Installing driveBiSync wallpapers cron job.."
 (crontab -l; echo "$WALLPAPER_CRON") | crontab
 echo "Installed driveBiSync obsidian vaults cron jobs."
 
+# symlink sync script for anacrontab
+mkdir -p ~/.local/anacron
+ln -s "$SCRIPT_DIR"/wallpapers/driveBiSync_wallpapers.sh ~/.local/anacron/driveBiSync_wallpapers.sh
+
 # enable cronie service
 echo "Enabling cronie service.."
 sudo systemctl enable --now cronie
