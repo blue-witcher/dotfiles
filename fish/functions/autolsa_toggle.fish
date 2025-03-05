00763f34850377dc14f1a4375f1ae1a155a6f65e
Blue-Witcher <>
2025-03-05 23:42:12 +0100

    added terminal feedback

--- fish/functions/autolsa_toggle.fish
+++ fish/functions/autolsa_toggle.fish
@@ -2,10 +2,13 @@ function autolsa_toggle --description 'Write out the prompt'
     switch $AUTO_LS_ALL
         case ""
             set -g AUTO_LS_ALL 1
+            echo "--all ON"
         case 0
             set -g AUTO_LS_ALL 1
+            echo "--all ON"
         case 1
             set -g AUTO_LS_ALL 0
+            echo "--all OFF"
     end
 
 end
