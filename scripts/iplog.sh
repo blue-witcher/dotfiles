270ba4ab0598e88f84a96030d0ee0fd7f7839867
Blue-Witcher <>
2025-03-12 15:12:24 +0100

    revision 2

--- scripts/iplog.sh
+++ scripts/iplog.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env fish
-mkdir -p ~/.local/var/log/
+
+mkdir -p ~/.local/var/log
 
 while true
-	  begin; date +%m.%d\ %T
-      curl -s ifconfig.me ; echo '' ;end | tee -a ~/.local/var/log/iplog.txt
-      sleep 1s
+    begin
+        date
+        curl -s ifconfig.me
+        echo
+    end | tee -a ~/.local/var/log/iplog.txt
+    sleep 2s
 end
