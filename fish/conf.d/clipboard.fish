ff20a58dbb20d36fbf8d3e8cac6674113f658ca9
Blue-Witcher <>
2025-03-17 13:07:32 +0100

    changed kitten clipboard abbr to wl-copy

--- fish/conf.d/clipboard.fish
+++ fish/conf.d/clipboard.fish
@@ -1,5 +1,5 @@
 if status is-interactive
-    if test $KITTY_PID
+    if test # $KITTY_PID # disabled since I like wl-copy more
         abbr -a cb 'kitten clipboard'
     else
         abbr -a cb 'wl-copy'
