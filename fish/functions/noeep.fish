036338854c0541a667e55cbd53d3d7b5876fdd70
Blue-Witcher <>
2025-03-23 16:44:10 +0100

    add user defined reason to noeep

--- fish/functions/noeep.fish
+++ fish/functions/noeep.fish
@@ -1,4 +1,4 @@
-function noeep --wraps='systemd-inhibit --what sleep --who "$USER" --why "no eep" -- sleep infinity' --description 'alias noeep systemd-inhibit --what sleep --who "$USER" --why "no eep" -- sleep infinity'
-  systemd-inhibit --what sleep --who "$USER" --why "no eep" -- sleep infinity $argv
+function noeep --wraps='systemd-inhibit --what sleep --who "no eep" --why "" -- sleep infinity' --description 'alias noeep systemd-inhibit --what sleep --who "no eep" --why "" -- sleep infinity'
+  systemd-inhibit --what sleep --who "no eep" --why "$argv" -- sleep infinity
         
 end
