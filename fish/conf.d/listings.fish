a79b7a553a508e41929eac3a7239c6888aa88358
Blue-Witcher <>
2025-03-31 06:29:42 +0200

    add more unnecessary eza abbreviations

--- fish/conf.d/listings.fish
+++ fish/conf.d/listings.fish
@@ -2,9 +2,14 @@ if status is-interactive
     if type -q eza
         abbr -a ls1 'ls -1'
         abbr -a lsa 'ls -a'
+        abbr -a lsa1 'ls -a1'
         abbr -a lt 'ls --tree'
         abbr -a llt 'ls -Tl'
         abbr -a lta 'ls -Ta'
         abbr -a llta 'll --tree'
+        abbr -a lsr 'ls -R'
+        abbr -a llr 'll -R'
+        abbr -a lsar 'ls -aR'
+        abbr -a lsr1 'ls -R1'
     end
 end
