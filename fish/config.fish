0ed0ef0dd318e7c48d118e64a0c3dc8651378fbd
Blue-Witcher <>
2025-04-29 03:56:47 +0200

    only add ~/.local/bin to $PATH when fish status=is-interactive

--- fish/config.fish
+++ fish/config.fish
@@ -3,11 +3,11 @@ set -x MICRO_TRUECOLOR 1 # settings micro_truecolor to 0 is also cool, makes cat
 set -x GIT_ASKPASS '/usr/bin/ksshaskpass'
 # set -x SSH_ASKPASS '/usr/bin/ksshaskpass'
 # set -x SSH_ASKPASS_REQUIRE 'prefer'
-fish_add_path ~/.local/bin
 
 if status is-interactive
 # Commands to run in interactive sessions can go here
 # Environment variable for use only in interactive shell or processes called from interactive shell
+    fish_add_path ~/.local/bin
     set -x EDITOR micro
     set -g fish_greeting \
 '              ⠀⠀⠀⢠⠏⡆⠀⠀⠀⠀⠀⢀⣀⣤⣤⣤⣀⡀
