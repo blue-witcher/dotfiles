6fa5629e1690cf177136822a2ba94024d03abd62
Blue-Witcher <>
2025-04-30 21:49:43 +0200

    add error when item is already in list

--- fish/functions/add2.fish
+++ fish/functions/add2.fish
@@ -2,6 +2,8 @@ function add2 --wraps='set -a' --description='add values to variable if not alre
     for i in $argv[2..]
         if not contains $i $$argv[1]
             set -a $argv[1] $i
+        else
+            return 1
         end
     end
         
