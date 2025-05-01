64538572b1b131740282a41ca227151fd17b515d
Blue-Witcher <>
2025-05-01 15:14:43 +0200

    return 1 if _some_ of the values were already in list but still check all of them

--- fish/functions/add2.fish
+++ fish/functions/add2.fish
@@ -2,7 +2,13 @@ function add2 --wraps='set -a' --description='add values to variable if not alre
     for i in $argv[2..]
         if not contains $i $$argv[1]
             set -a $argv[1] $i
+        else
+            set --function present 1
         end
+    end
+
+    if test $present = 1
+        return 1
     end
         
 end
