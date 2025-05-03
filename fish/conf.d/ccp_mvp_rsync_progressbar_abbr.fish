b8e68ea481cab598c64a371b48207ebda3fef20b
Blue-Witcher <>
2025-05-03 02:59:56 +0200

    add human readable flag

--- fish/conf.d/ccp_mvp_rsync_progressbar_abbr.fish
+++ fish/conf.d/ccp_mvp_rsync_progressbar_abbr.fish
@@ -1,6 +1,6 @@
 if status is-interactive
     if type -q rsync
-        abbr -a cpp 'rsync --info=progress2 -a'
-        abbr -a mvp 'rsync --remove-source-files --info=progress2 -a'
+        abbr -a cpp 'rsync --info=progress2 -ha'
+        abbr -a mvp 'rsync --remove-source-files --info=progress2 -ha'
     end
 end
