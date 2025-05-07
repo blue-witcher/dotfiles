d1e4169e1bbaf2b34a99eb2b3cb385890f85dc77
Blue-Witcher <>
2025-05-07 23:07:19 +0200

    add advanced copy/move support - acp > 'advcp -g' abbr - amv > 'advmv -g' abbr - install advcpmv script

---
+++ fish/conf.d/advcpmv.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    if type -q advcp
+        abbr -a acp 'advcp -g'
+    end
+    if type -q advmv
+        abbr -a amv 'advmv -g'
+    end
+    
+end
---
+++ scripts/install_advcpmv.fish
@@ -0,0 +1,33 @@
+#!/usr/bin/env fish
+
+# check dependencies
+if not type -q patch
+    echo "Dependency \"patch\" not installed"
+    return
+end
+if not type -q gcc
+    echo "Dependency \"gcc\" not installed"
+    return
+end
+
+# compile
+curl https://raw.githubusercontent.com/jarun/advcpmv/master/install.sh --create-dirs -o ./advcpmv/install.sh
+and begin
+    cd advcpmv
+    and sh install.sh
+
+end
+
+# place files
+read -c 'y' -P 'Place binaries in "~/.local/bin"? [y/n]: ' -l q1
+if test (string sub -l 1 $q1) = y
+    mkdir --parents ~/.local/bin
+    and cp advcp advmv ~/.local/bin/
+end
+
+# delete working dir
+read -c 'y' -P 'Delete working directory? [y/n]: ' -l q2
+if test (string sub -l 1 $q2) = y
+    cd ..
+    and rm -r ./advcpmv/
+end
