50f2b910a9b90d3a69f17a62210c16c438a2f016
Blue-Witcher <>
2025-05-16 19:20:37 +0200

    add previewer

--- lf/install_lf_config.sh
+++ lf/install_lf_config.sh
@@ -8,7 +8,10 @@ mkdir -p ~/.config/lf
 # link files
 ln -s "$SCRIPT_DIR"/lfrc       ~/.config/lf/
 ln -s "$SCRIPT_DIR"/lf.fish    ~/.config/fish/functions/
+ln -s "$SCRIPT_DIR"/cleaner    ~/.config/lf/
+ln -s "$SCRIPT_DIR"/previewer  ~/.config/lf/
 
 # download icons and colors
 wget https://github.com/gokcehan/lf/raw/refs/heads/master/etc/icons.example -O ~/.config/lf/icons
 wget https://github.com/gokcehan/lf/raw/refs/heads/master/etc/colors.example -O ~/.config/lf/colors 
+wget https://raw.githubusercontent.com/duganchen/kitty-pistol-previewer/main/vidthumb -O ~/.config/lf/vidthumb && chmod u+x ~/.config/lf/vidthumb
