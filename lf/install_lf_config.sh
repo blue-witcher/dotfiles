41f2f4b91840958486cd89b84091d7004b33cdc6
blue-witcher <171081467+blue-witcher@users.noreply.github.com>
2025-05-16 07:35:01 +0200

    update lf dotfiles

--- lf/install_lf_config.sh
+++ lf/install_lf_config.sh
@@ -8,3 +8,7 @@ mkdir -p ~/.config/lf
 # link files
 ln -s "$SCRIPT_DIR"/lfrc       ~/.config/lf/
 ln -s "$SCRIPT_DIR"/lf.fish    ~/.config/fish/functions/
+
+# download icons and colors
+wget https://github.com/gokcehan/lf/raw/refs/heads/master/etc/icons.example -O ~/.config/lf/icons
+wget https://github.com/gokcehan/lf/raw/refs/heads/master/etc/colors.example -O ~/.config/lf/colors 
