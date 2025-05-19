c705d8967b72a1ca0a578efcd64e74d2384e64cb
Blue-Witcher <>
2025-05-20 01:46:50 +0200

    move line around

--- lf/install_lf_config.sh
+++ lf/install_lf_config.sh
@@ -2,15 +2,14 @@
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # make directories
-mkdir -p ~/.config/fish/functions
 mkdir -p ~/.config/lf
+mkdir -p ~/.config/fish/functions
 
 # link files
 ln -s "$SCRIPT_DIR"/lfrc       ~/.config/lf/
 ln -s "$SCRIPT_DIR"/lf.fish    ~/.config/fish/functions/
 ln -s "$SCRIPT_DIR"/cleaner    ~/.config/lf/
 ln -s "$SCRIPT_DIR"/previewer  ~/.config/lf/
-ln -s "$SCRIPT_DIR"/zoxide.sh  ~/.config/lf/
 
 # download icons and colors
 wget https://github.com/gokcehan/lf/raw/refs/heads/master/etc/icons.example -O ~/.config/lf/icons
