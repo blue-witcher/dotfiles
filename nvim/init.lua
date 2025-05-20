af438ed65d6d5dfa12d32498d72b8bdd899f2c35
Blue-Witcher <>
2025-05-20 06:52:56 +0200

    I wanna learn vim now (oh no)

---
+++ nvim/init.lua
@@ -0,0 +1,8 @@
+-- enable line numbers
+vim.wo.number = true
+
+-- cursor remappings
+vim.keymap.set({'n', 'v'}, 'n', '<left>')
+vim.keymap.set({'n', 'v'}, 'e', '<down>')
+vim.keymap.set({'n', 'v'}, 'i', '<up>')
+vim.keymap.set({'n', 'v'}, 'o', '<right>')
