026d276f06bcf6a5f9ddac5e9ec1072dd175bd38
Blue-Witcher <>
2025-05-20 07:36:15 +0200

    add more keybinds and relative numbers

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,12 @@
 -- enable line numbers
-vim.wo.number = true
+vim.opt.number = true
+vim.opt.relativenumber = true
 
 -- cursor remappings
 vim.keymap.set({'n', 'v'}, 'n', '<left>')
 vim.keymap.set({'n', 'v'}, 'e', '<down>')
 vim.keymap.set({'n', 'v'}, 'i', '<up>')
 vim.keymap.set({'n', 'v'}, 'o', '<right>')
+vim.keymap.set({'n', 'v'}, 'k', '<insert>')
+vim.keymap.set('n', 'K', 'I')
+
