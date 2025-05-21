dfc5f58dc4aeb924eec048f6ab99a408fef469f2
Blue-Witcher <>
2025-05-21 11:36:09 +0200

    cool new gitc abbr for git commit

--- fish/conf.d/git.fish
+++ fish/conf.d/git.fish
@@ -4,5 +4,6 @@ if status is-interactive
         abbr -a gitf 'git fetch'
         abbr -a gitfs 'git fetch ; git status'
         abbr -a gitl 'git log HEAD origin/HEAD'
+		abbr -a gitc --set-cursor 'git commit -m\'%'
     end
 end
