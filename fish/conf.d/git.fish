cc987a7627da57545276153a303963dd1eaa3f6e
Blue-Witcher <>
2025-05-21 22:54:02 +0200

    add ' to end of gitc

--- fish/conf.d/git.fish
+++ fish/conf.d/git.fish
@@ -4,6 +4,6 @@ if status is-interactive
         abbr -a gitf 'git fetch'
         abbr -a gitfs 'git fetch ; git status'
         abbr -a gitl 'git log HEAD origin/HEAD'
-		abbr -a gitc --set-cursor 'git commit -m\'%'
+		abbr -a gitc --set-cursor 'git commit -m\'%\''
     end
 end
