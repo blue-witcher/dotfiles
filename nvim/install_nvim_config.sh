a66f8b9bb27914b58985b2e45a910bf61b41dffc
Blue-Witcher <>
2025-05-25 14:12:09 +0200

    add mkdir command

--- nvim/install_nvim_config.sh
+++ nvim/install_nvim_config.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
+mkdir -p ~/.config/nvim
 ln -s "$SCRIPT_DIR"/init.lua     ~/.config/nvim/
 ln -s "$SCRIPT_DIR"/lua          ~/.config/nvim/
