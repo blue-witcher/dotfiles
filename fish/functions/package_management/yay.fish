db063ae62c3277b20bd3ded705255f13328dbfed
Blue-Witcher <>
2025-06-01 20:55:20 +0200

    add fallback to pacman on arch when paru is not present

--- fish/functions/package_management/yay.fish
+++ fish/functions/package_management/yay.fish
@@ -1,9 +1,14 @@
-if type -q paru
+if command -q paru
     function yay --wraps=paru --description 'alias yay=paru'
       paru $argv
     end
 
-else if type -q zypper
+else if command -q pacman
+	function yay --wraps=zypper --description 'alias yay=zypper'
+	  pacman $argv
+	end
+
+else if command -q zypper
     function yay --wraps=zypper --description 'alias yay=zypper'
       zypper $argv
     end
--- fish/functions/package_management/yayy.fish
+++ fish/functions/package_management/yayy.fish
@@ -1,8 +1,13 @@
-if type -q zypper
+if command -q zypper
     function yayy --wraps='zypper' --description 'alias yayy=sudo zypper'
       sudo zypper $argv
     end
 
+else if command -q pacman ;and not command -q paru
+	function yayy --wraps=pacman --description 'alias yayy=sudo pacman'
+		sudo pacman $argv
+	end
+	
 else
     function yayy
         echo 'Function not applicable.'
