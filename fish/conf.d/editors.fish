5af74a5b7feba7246da5df23358a4ad2f06b5fcd
Blue-Witcher <>
2025-06-05 01:28:06 +0200

    renamed nvim.fish to more general editors.fish

---
+++ fish/conf.d/editors.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    if command -q nvim
+        abbr -a vi nvim
+    else if command -q vim
+        abbr -a vi vim
+    end
+
+    if command -q ms-edit
+        abbr -a edit ms-edit
+    end
+end
--- fish/conf.d/nvim.fish
+++
@@ -1,8 +0,0 @@
-if status is-interactive
-	if command -q nvim
-		abbr -a vi nvim
-	else if command -q vim
-		abbr -a vi vim
-	end
-end
-	
