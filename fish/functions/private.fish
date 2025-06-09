0d3db84bd59d26a5784deddb1bc0ae55b7a22b79
Blue-Witcher <>
2025-06-10 01:20:35 +0200

    fix --help command formatting

--- fish/functions/private.fish
+++ fish/functions/private.fish
@@ -1,6 +1,25 @@
 function private --description 'Toggle fish private mode'
+    argparse 'h/help' 's/status' -- $argv
+# help flag
+    if set -q _flag_help
+        echo -se '-h/--help' '\n' '-s/--status'
+        return
+    end
+
     set -f _status (set -q fish_private_mode; echo $status)
 
+# Status flag
+    if set -q _flag_status
+        switch "$_status"
+            case 1
+                echo "Private mode is off."
+            case 0
+                echo "Private mode is on."
+        end
+        return
+    end
+
+# main function
     switch "$_status"
         case 1
             set -g fish_private_mode 1
