fbdab4d7c12395d76ed5dc9ea9d9561043f84ff7
Blue-Witcher <>
2025-06-09 20:03:52 +0200

    fixed private.fish

--- fish/functions/private.fish
+++ fish/functions/private.fish
@@ -1,14 +1,13 @@
 function private --description 'Toggle fish private mode'
-	switch "$fish_private_mode"
-		case ''
-			set -g fish_private_mode 1
+    set -f _status (set -q fish_private_mode; echo $status)
+
+    switch "$_status"
+        case 1
+            set -g fish_private_mode 1
 			echo "Fish private mode is now on."
-		case '0'
-			set -g fish_private_mode 1  
-			echo "Fish private mode is now on."
-		case '1'
+		case 0
 			set -eg fish_private_mode
 			echo "Fish private mode is now off."
-	end
+    end
 
 end
