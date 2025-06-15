0ce46c209b4878cf140b8cf2aff25707181d2ccd
Blue-Witcher <>
2025-06-16 00:16:19 +0200

    add zoxide database abbrs

--- fish/conf.d/zoxide.fish
+++ fish/conf.d/zoxide.fish
@@ -1,6 +1,8 @@
 if status is-interactive
     if command -q zoxide
         zoxide init fish | source
+        abbr -a dse                 'zoxide query -i'
+        abbr -a f   --set-cursor    'cd (zoxide query -i %)'
 
     end
 
