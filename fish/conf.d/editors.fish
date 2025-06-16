8b552dcea036723b30291d99c979e22949808665
Blue-Witcher <>
2025-06-16 03:40:25 +0200

    add vim to nvim abbr

--- fish/conf.d/editors.fish
+++ fish/conf.d/editors.fish
@@ -1,11 +1,15 @@
 if status is-interactive
     if command -q nvim
-        abbr -a vi nvim
+        abbr -a vi  nvim
+        abbr -a vim nvim
+
     else if command -q vim
         abbr -a vi vim
+
     end
 
     if command -q ms-edit
         abbr -a edit ms-edit
+
     end
 end
