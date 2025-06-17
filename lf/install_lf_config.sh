90df98a3ef9b9609eb973216fd611adb3a69261a
Blue-Witcher <>
2025-06-17 21:47:15 +0200

    add no-clobber option

--- lf/install_lf_config.sh
+++ lf/install_lf_config.sh
@@ -12,6 +12,6 @@ ln -s "$SCRIPT_DIR"/cleaner    ~/.config/lf/
 ln -s "$SCRIPT_DIR"/previewer  ~/.config/lf/
 
 # download icons and colors
-wget https://github.com/gokcehan/lf/raw/refs/heads/master/etc/icons.example -O ~/.config/lf/icons
-wget https://github.com/gokcehan/lf/raw/refs/heads/master/etc/colors.example -O ~/.config/lf/colors 
-wget https://raw.githubusercontent.com/duganchen/kitty-pistol-previewer/main/vidthumb -O ~/.config/lf/vidthumb && chmod u+x ~/.config/lf/vidthumb
+wget -nc https://github.com/gokcehan/lf/raw/refs/heads/master/etc/icons.example -O ~/.config/lf/icons
+wget -nc https://github.com/gokcehan/lf/raw/refs/heads/master/etc/colors.example -O ~/.config/lf/colors 
+wget -nc https://raw.githubusercontent.com/duganchen/kitty-pistol-previewer/main/vidthumb -O ~/.config/lf/vidthumb && chmod u+x ~/.config/lf/vidthumb
