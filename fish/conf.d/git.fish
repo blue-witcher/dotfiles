3eaedfd51fccfd6cef1d9c52040686e070afb2fa
Blue-Witcher <>
2025-06-18 22:50:37 +0200

    add gitco -> 'git commit' abbr

--- fish/conf.d/git.fish
+++ fish/conf.d/git.fish
@@ -1,12 +1,13 @@
 if status is-interactive
     if command -q git
-        abbr -a gits 'git status'
-        abbr -a gitf 'git fetch'
-        abbr -a gitfs 'git fetch ; git status'
-        abbr -a gitl 'git log HEAD origin/HEAD'
-		abbr -a gitc --set-cursor 'git commit -m"%"'
-		abbr -a gitr 'git restore'
-		abbr -a gitd 'git diff'
-		abbr -a gita 'git add'
+        abbr -a gits                'git status'
+        abbr -a gitf                'git fetch'
+        abbr -a gitfs               'git fetch ; git status'
+        abbr -a gitl                'git log HEAD origin/HEAD'
+        abbr -a gitc --set-cursor   'git commit -m"%"'
+        abbr -a gitco               'git commit'
+        abbr -a gitr                'git restore'
+        abbr -a gitd                'git diff'
+        abbr -a gita                'git add'
     end
 end
