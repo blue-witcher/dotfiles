3a4723ee7b631703d9c3f383acf264817fcbb897
Blue-Witcher <>
2025-06-18 22:53:30 +0200

    add empty line between fetch and status

--- fish/conf.d/git.fish
+++ fish/conf.d/git.fish
@@ -2,7 +2,7 @@ if status is-interactive
     if command -q git
         abbr -a gits                'git status'
         abbr -a gitf                'git fetch'
-        abbr -a gitfs               'git fetch ; git status'
+        abbr -a gitfs               'git fetch ; echo ;  git status'
         abbr -a gitl                'git log HEAD origin/HEAD'
         abbr -a gitc --set-cursor   'git commit -m"%"'
         abbr -a gitco               'git commit'
