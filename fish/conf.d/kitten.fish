b127897e57a445a0b0522b2300077c8091c85167
Blue-Witcher <>
2025-06-18 06:32:14 +0200

    change kitten terminal test and add new kitten abbrs

--- fish/conf.d/kitten.fish
+++ fish/conf.d/kitten.fish
@@ -1,9 +1,11 @@
 if status is-interactive
-    if test $KITTY_PID
-        abbr -a icat 'kitten icat'
-        abbr -a kb 'kitten clipboard'
-        abbr -a kps 'kitten clipboard -g'
-        abbr -a ssh 'kitten ssh'
+    if test "$(kitten query-terminal name)" = 'name: kitty'
+        abbr -a icat    'kitten icat'
+        abbr -a kb      'kitten clipboard'
+        abbr -a kps     'kitten clipboard -g'
+        abbr -a ssh     'kitten ssh'
+        abbr -a ed      'kitten edit-in-kitty'
+        abbr -a kiff    'kitten diff'
+        abbr -a trans   'kitten transfer -c -d'
     end
-
 end
