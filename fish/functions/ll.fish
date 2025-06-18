8941ad69572da1297dce66944715454da18e7e2d
Blue-Witcher <>
2025-06-18 05:35:13 +0200

    change type to command

--- fish/functions/ll.fish
+++ fish/functions/ll.fish
@@ -1,4 +1,4 @@
-if type -q eza
+if command -q eza
     function ll --wraps='eza --color=always --group-directories-first --icons --smart-group --git --links --no-quotes --hyperlink -al' --description 'alias ll=ls -al'
         ls -al $argv
             
--- fish/functions/ls.fish
+++ fish/functions/ls.fish
@@ -1,4 +1,4 @@
-if type -q eza
+if command -q eza
     function ls --wraps='eza --color=always --group-directories-first --icons --smart-group --git --links --no-quotes --hyperlink' --description 'alias ls=eza --color=always --group-directories-first --icons --smart-group --git --links --no-quotes --hyperlink'
         eza --color=always --group-directories-first --icons --smart-group --git --links --no-quotes --hyperlink $argv
     end
