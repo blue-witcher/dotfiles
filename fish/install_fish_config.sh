70af6a768f6e339a544052b37b7a075c34b71a4b
Blue-Witcher <>
2025-06-21 19:38:09 +0200

    don't overwrite existing backups on reruns and don't symlink into existing symlink

--- fish/install_fish_config.sh
+++ fish/install_fish_config.sh
@@ -2,14 +2,14 @@
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # move existing config.fish, functions/ and conf.d/ out of the way if they exist
-mv ~/.config/fish/config.fish   ~/.config/fish/config.fish.backup
-mv ~/.config/fish/functions     ~/.config/fish/old_functions
-mv ~/.config/fish/conf.d        ~/.config/fish/old_conf.d
+mv -Tn  ~/.config/fish/config.fish  ~/.config/fish/config.fish.backup
+mv -Tn  ~/.config/fish/functions    ~/.config/fish/old_functions
+mv -Tn  ~/.config/fish/conf.d       ~/.config/fish/old_conf.d
 
 # symlink config.fish, functions/ and conf.d/
-ln -s "$SCRIPT_DIR"/config.fish ~/.config/fish/
-ln -s "$SCRIPT_DIR"/functions   ~/.config/fish/functions
-ln -s "$SCRIPT_DIR"/conf.d      ~/.config/fish/conf.d
+ln -s   "$SCRIPT_DIR"/config.fish   ~/.config/fish/
+ln -sT  "$SCRIPT_DIR"/functions     ~/.config/fish/functions
+ln -sT  "$SCRIPT_DIR"/conf.d        ~/.config/fish/conf.d
 
 # set keybinds
 fish -c 'fish_hybrid_key_bindings'
