0ed4b6921e4f4c8f487df6355060253605635456
Blue-Witcher <>
2025-06-22 02:46:35 +0200

    prefer kitten clipboard over wl-clipboard when possible

--- fish/conf.d/clipboard.fish
+++ fish/conf.d/clipboard.fish
@@ -1,5 +1,9 @@
 if status is-interactive
-    abbr -a cb 'wl-copy'
-    abbr -a pst 'wl-paste'
-
+    if string match -q -- '*kitty*' "$TERM" ;and command -q kitten
+        abbr -a cb 'kitten clipboard'
+        abbr -a pst 'kitten clipboard -g'
+    else if command -q wl-copy
+        abbr -a cb 'wl-copy'
+        abbr -a pst 'wl-paste'
+    end
 end
--- fish/conf.d/kitten.fish
+++ fish/conf.d/kitten.fish
@@ -1,8 +1,6 @@
 if status is-interactive
     if string match -q -- '*kitty*' "$TERM" ;and command -q kitten
         abbr -a icat    'kitten icat'
-        abbr -a kb      'kitten clipboard'
-        abbr -a kps     'kitten clipboard -g'
         abbr -a ssh     'kitten ssh'
         abbr -a ed      'kitten edit-in-kitty'
         abbr -a kiff    'kitten diff'
