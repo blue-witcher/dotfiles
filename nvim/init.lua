0225fd57bb2018463805dc006d392582efc33a19
Blue-Witcher <>
2025-06-26 01:23:10 +0200

    add descriptions to keybinds

--- nvim/init.lua
+++ nvim/init.lua
@@ -22,9 +22,9 @@ set.relativenumber = true
 require 'colemak'
 
 --other remaps
-vim.keymap.set({'n', 'v'}, '<leader>y', '"+y')
-vim.keymap.set({'n', 'v'}, '<leader>p', '"+p')
-vim.keymap.set({'n', 'v'}, '<leader>x', '"+x')
+vim.keymap.set({'n', 'v'}, '<leader>y', '"+y', { desc = "Yank into system clipboard" })
+vim.keymap.set({'n', 'v'}, '<leader>p', '"+p', { desc = "Paste from system clipboard" })
+vim.keymap.set({'n', 'v'}, '<leader>x', '"+x', { desc = "Cut into system clipboard" })
 vim.keymap.set({'n', 'v'}, ';', ':')
 vim.keymap.set('i', '<s-tab>', '<bs>')
 
