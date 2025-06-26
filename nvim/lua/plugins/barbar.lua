404b144a042fa953fb07f1d0382e15ef98623817
Blue-Witcher <>
2025-06-26 05:16:24 +0200

    closing window also closes tab

--- nvim/lua/plugins/barbar.lua
+++ nvim/lua/plugins/barbar.lua
@@ -3,8 +3,18 @@ return {
         'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
         'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
     },
-    init = function() vim.g.barbar_auto_setup = false end,
-    opts = {
+    init = function() vim.g.barbar_auto_setup = false
+        -- closing window also closes buffer and thus tab
+        vim.api.nvim_create_autocmd("WinClosed", {
+            callback = function(tbl)
+                if vim.api.nvim_buf_is_valid(tbl.buf) then
+                    vim.api.nvim_buf_delete(tbl.buf, {})
+                end
+            end,
+            group = vim.api.nvim_create_augroup("barbar_close_buf", {}),
+        })
+    end,
+        opts = {
         -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
         -- animation = true,
         -- insert_at_start = true,
