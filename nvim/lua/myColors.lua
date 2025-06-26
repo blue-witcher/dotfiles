534725b8d2e2302587606e4a9b29e7b8f1caba76
Blue-Witcher <>
2025-06-26 17:56:10 +0200

    add more colors

--- nvim/lua/myColors.lua
+++ nvim/lua/myColors.lua
@@ -1,3 +1,4 @@
-vim.cmd('highlight Normal guibg=#0a0a10')
+vim.cmd('highlight Normal guibg=#0a0a10 guifg=#dbdbdb')
+vim.cmd('highlight NonText guibg=#0a0a10 guifg=#0a0a10')
 vim.cmd('highlight CursorLine guibg=#0e1e22')
 vim.cmd('highlight CursorLineNr guifg=#ae81ff')
