82d08ecb8728963ae76501f6f313b796d681b838
Blue-Witcher <>
2025-06-26 05:43:42 +0200

    add gitsigns to barbar

--- nvim/lua/plugins/barbar.lua
+++ nvim/lua/plugins/barbar.lua
@@ -1,8 +1,8 @@
 return {
     { 'romgrk/barbar.nvim', dependencies = {
         'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
-        'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
-    },
+        'nvim-tree/nvim-web-devicons', }, -- OPTIONAL: for file icons
+
     init = function() vim.g.barbar_auto_setup = false
         -- closing window also closes buffer and thus tab
         vim.api.nvim_create_autocmd("WinClosed", {
@@ -14,11 +14,18 @@ return {
             group = vim.api.nvim_create_augroup("barbar_close_buf", {}),
         })
     end,
-        opts = {
+
+    opts = {
         -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
         -- animation = true,
         -- insert_at_start = true,
         -- …etc.
-    },
-},
+        icons = {
+            gitsigns = {
+                added = {enabled = true, icon = '+'},
+                changed = {enabled = true, icon = '~'},
+                deleted = {enabled = true, icon = '-'},
+            },
+        },
+    },},
 }
