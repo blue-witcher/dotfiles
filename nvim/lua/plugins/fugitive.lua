480d13d5025c9376a99041573b53907b967b5c8e
Blue-Witcher <>
2025-06-26 05:36:52 +0200

    add g/git -> Git command alias

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -1,3 +1,7 @@
 return {
-	{ "tpope/vim-fugitive", name = 'fugitive' }
+	{ "tpope/vim-fugitive", name = 'fugitive',
+    init = function()
+        vim.keymap.set('ca', 'git', 'Git')
+        vim.keymap.set('ca', 'g', 'Git')
+    end }
 }
