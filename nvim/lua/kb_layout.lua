8d4930b262458b04fe68cb7592ed30e3460fcc24
Blue-Witcher <>
2025-06-27 09:44:15 +0200

    move rebinds to their own files so you can load them manually if the if function fails

--- nvim/lua/kb_layout.lua
+++ nvim/lua/kb_layout.lua
@@ -3,30 +3,9 @@
 local kb_layout = os.getenv('kb_layout')
 
 if kb_layout == 'colemak-dh' then
-    -- cursor
-    vim.keymap.set({'n', 'v'}, 'n', '<left>')
-    vim.keymap.set({'n', 'v'}, 'N', 'H')
-    vim.keymap.set({'n', 'v'}, 'e', '<down>')
-    vim.keymap.set({'n', 'v'}, 'E', 'J')
-    vim.keymap.set({'n', 'v'}, 'i', '<up>')
-    vim.keymap.set({'n', 'v'}, 'I', 'K')
-    vim.keymap.set({'n', 'v'}, 'o', '<right>')
-    vim.keymap.set({'n', 'v'}, 'O', 'L')
-    vim.keymap.set({'n', 'v'}, 'j', 'n')
-    vim.keymap.set({'n', 'v'}, 'J', 'N')
-    vim.keymap.set({'n', 'v'}, 'h', 'e')
-    vim.keymap.set({'n', 'v'}, 'H', 'E')
-    vim.keymap.set({'n', 'v'}, 'l', 'i')
-    vim.keymap.set({'n', 'v'}, 'L', 'I')
-    vim.keymap.set({'n', 'v'}, 'k', 'o')
-    vim.keymap.set({'n', 'v'}, 'K', 'O')
-
-    vim.keymap.set({'n', 'v'}, ';', ':')
-    vim.keymap.set({'n', 'v'}, '<C-/>', 'gcc', { remap = true })
+    require 'colemak-dh'
 elseif kb_layout == 'qwertz' then
-    vim.keymap.set({'n', 'v'}, '.', ':')
-    vim.keymap.set({'n', 'v'}, '<C-->', 'gcc', { remap = true })
+    require 'qwertz'
 elseif kb_layout == 'qwerty' or kb_layout == nil then
-    vim.keymap.set({'n', 'v'}, ';', ':')
-    vim.keymap.set({'n', 'v'}, '<C-/>', 'gcc', { remap = true })
+    require 'qwerty'
 end
