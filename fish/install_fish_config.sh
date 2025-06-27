7d5582c377c2473d11f638e2ad9c57b8aaad2a48
Blue-Witcher <>
2025-06-27 05:39:14 +0200

    added option for qwertz

--- fish/install_fish_config.sh
+++ fish/install_fish_config.sh
@@ -19,12 +19,14 @@ fish -c 'fish_config theme save Dracula'
 
 # set keyboard layout
 fish -c "
-read -P 'Which keyboard layout do you use? Colemak-(D)H or (q)werty?' layout
+read -P 'Which keyboard layout do you use? Colemak-(D)H, qwert(y) or qwert(z)?' layout
 switch \"\$(string lower \$layout)\"
-    case 'q*'
+    case 'y*'
         set -Ux kb_layout 'qwerty'
     case 'd*'
         set -Ux kb_layout 'colemak-dh'
+    case 'z*'
+        set -Ux kb_layout 'qwertz'
     case '*'
         echo 'No keyboard layout set.'
 end"
