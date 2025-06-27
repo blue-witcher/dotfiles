246833533c4f518a1d3141b22359687ef17444c6
Blue-Witcher <>
2025-06-27 12:55:57 +0200

    add window focus binds

--- nvim/lua/colemak-dh.lua
+++ nvim/lua/colemak-dh.lua
@@ -16,5 +16,12 @@ vim.keymap.set({'n', 'v'}, 'L', 'I')
 vim.keymap.set({'n', 'v'}, 'k', 'o')
 vim.keymap.set({'n', 'v'}, 'K', 'O')
 
+-- other stuff
 vim.keymap.set({'n', 'v'}, ';', ':')
 vim.keymap.set({'n', 'v'}, '<C-/>', 'gcc', { remap = true })
+
+-- move window focus
+vim.keymap.set('n', '<C-n>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-o>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-e>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-i>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
