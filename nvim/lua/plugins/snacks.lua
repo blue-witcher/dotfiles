99979459b0c242c55e25a4b3ffb5e02c7bf6cbb9
Blue-Witcher <>
2025-06-28 06:51:19 +0200

    enabled terminal, explorer and scroll

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -14,13 +14,18 @@ return {
                     ["<Esc>"] = { "close", mode = { "n", "i" } },
                 },},},
             },
+            explorer = {enabled = true},
             quickfile = {enabled = true},
+            terminal = {enabled = true},
+            scroll = {enabled = true},
         },
         keys = {
             { "<leader>ff", function() Snacks.picker.files() end, desc = "Find Files" },
             { "<leader>fg", function() Snacks.picker.git_files() end, desc = "Find Git Files" },
             { "<leader><space>", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
             { "<leader>fc", function() Snacks.picker.colorschemes() end, desc = "Colorschemes" },
+            { "<leader>e", function() Snacks.explorer.open() end, desc = "Open explorer" },
+            { "<leader>t", function() Snacks.terminal.toggle() end, desc = "Toggle terminal" },
         },
     }
 }
