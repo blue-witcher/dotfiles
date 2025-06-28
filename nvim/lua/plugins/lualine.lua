fe79021e4fcfa2e9aac83a42a444925c77d9dc7a
Blue-Witcher <>
2025-06-28 05:16:07 +0200

    set filename to relative path

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -7,6 +7,14 @@ return {
               theme = require('lualine.themes.myTheme'),
               -- theme = 'dracula',
           },
+          sections = {
+              lualine_c = {
+                  {
+                      'filename',
+                      path = 1,
+                  },
+              },
+          },
       }
     end,
   },
