fcdcd0a2e073bd4a1956682cbffd51abb2577c8b
Blue-Witcher <>
2025-06-30 00:19:09 +0200

    add a bunch of command abbreviatons

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -1,7 +1,14 @@
 return {
 	{ "tpope/vim-fugitive", name = 'fugitive',
-    init = function()
+    config = function()
         vim.keymap.set('ca', 'git', 'Git')
         vim.keymap.set('ca', 'g', 'Git')
+        vim.keymap.set('ca', 'gitr', 'Git restore')
+        vim.keymap.set('ca', 'gits', 'Git status')
+        vim.keymap.set('ca', 'gitc', 'Git commit')
+        vim.keymap.set('ca', 'gitca', 'Git commit --amend')
+        vim.keymap.set('ca', 'gitd', 'Git diff')
+        vim.keymap.set('ca', 'gitl', 'Git log')
+        vim.keymap.set('ca', 'gita', 'Git add')
     end }
 }
