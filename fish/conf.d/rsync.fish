982872a1e009cf390d2b6287bd423a79bbee1e23
Blue-Witcher <>
2025-07-16 00:28:12 +0200

    remove --info flag

--- fish/conf.d/rsync.fish
+++ fish/conf.d/rsync.fish
@@ -1,6 +1,6 @@
 if status is-interactive
     if command -q rsync
-        set -l -- flags --human-readable --info all
+        set -l -- flags --human-readable
         abbr -a rs      "rsync $flags"
         abbr -a rsync   "rsync $flags"
     end
