d5ce18ddf87e9350a866b48ab805f35bd4815ff7
Blue-Witcher <>
2025-07-19 00:57:37 +0200

    rewrote private function (started by rewriting the insane --status flag)

--- fish/functions/private.fish
+++ fish/functions/private.fish
@@ -1,32 +1,36 @@
 function private --description 'Toggle fish private mode'
     argparse 'h/help' 's/status' -- $argv
-# help flag
-    if set -q _flag_help
+
+    # help flag
+    function __private_help
         echo -se '-h/--help' '\n' '-s/--status'
-        return
     end
 
-    set -f _status (set -q fish_private_mode; echo $status)
-
-# Status flag
-    if set -q _flag_status
-        switch "$_status"
-            case 1
-                echo "Private mode is off."
-            case 0
-                echo "Private mode is on."
+    # status flag
+    function __private_status
+        if set -q fish_private_mode
+            echo "Private mode is on."
+        else
+            echo "Private mode is off."
         end
-        return
     end
 
-# main function
-    switch "$_status"
-        case 1
-            set -g fish_private_mode 1
-			echo "Fish private mode is now on."
-		case 0
-			set -eg fish_private_mode
-			echo "Fish private mode is now off."
+    # main
+    function __private_main
+        if set -q fish_private_mode
+            set -ge fish_private_mode
+            echo "Fish private mode is now off."
+        else
+            set -g fish_private_mode
+            echo "Fish private mode is now on."
+        end
     end
 
+    if set -ql _flag_help
+        __private_help
+    else if set -ql _flag_status
+        __private_status
+    else
+        __private_main
+    end
 end
