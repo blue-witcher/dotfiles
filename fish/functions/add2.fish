7a6bfa235c9a7fbb53e9c9034afd18bc991b30de
Blue-Witcher <>
2025-07-22 02:32:42 +0200

    add help flag

--- fish/functions/add2.fish
+++ fish/functions/add2.fish
@@ -4,10 +4,10 @@ function add2 --description='add values to variable if not already in it'
     'If one or more items from could not be adde because of this, the function will return 1.' '\n'     \
     'add2 can also work on files. For this, use the -f/--file flag instead.'
 
-    argparse -x file,var f/file= v/var= -- $argv
+    argparse -x file,var 'f/file=' 'v/var=' 'h/help' -- $argv
     or begin echo -se $_help ;return ;end
 
-    if not set -q _flag_file ;and not set -q _flag_var
+    if not set -q _flag_file ;and not set -q _flag_var ;or set -ql _flag_help
         echo -se $_help
         return
     end
