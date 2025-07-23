ef11545fa7d940ba3e98ffd7ba00c6e97221c2f7
Blue-Witcher <>
2025-07-23 11:28:40 +0200

    added message when installing software

--- fish/functions/yay.fish
+++ fish/functions/yay.fish
@@ -1,16 +1,25 @@
 if command -q paru
     function yay --wraps=paru --description 'alias yay=paru'
-      paru $argv
+        if string match -q -- '-*' $argv
+            if string match -q -- '*-S*' $argv ;and not string match -q -- '-S*s*' $argv ;and not match -q -- '-S*i*' $argv
+                echo "Yay we're installing software!! :3"
+            end
+        else
+            echo "Yay we're installing software!! :3"
+        end
+        paru $argv
     end
 
 else if command -q pacman
-	function yay --wraps=zypper --description 'alias yay=zypper'
-	  pacman $argv
-	end
+    function yay --wraps=zypper --description 'alias yay=zypper'
+        if string match -q -- '-S*' $argv ;and not string match -q -- '-S*s*' $argv ;and not string match -q -- '-S*i*' $argv
+            echo "Yay we're installing software!! :3"
+        end
+    end
 
 else if command -q zypper
     function yay --wraps=zypper --description 'alias yay=zypper'
-      zypper $argv
+        zypper $argv
     end
 
 else if command -q nala
