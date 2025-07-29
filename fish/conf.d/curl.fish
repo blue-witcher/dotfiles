126e30ed76a4e08b6f966c0aaf769094370ee287
Blue-Witcher <>
2025-07-29 08:14:36 +0200

    better curl abbr

--- fish/conf.d/curl.fish
+++ fish/conf.d/curl.fish
@@ -1,3 +1,7 @@
 if status is-interactive ;and command -q curl
-    abbr -a curlo 'curl --remote-name-all'
+    set -l -- opts --skip-existing
+    abbr -a curl "curl $opts"
+    abbr -a --command curl -- '--se' '--skip-existing'
+    abbr -a --command curl -- '--nc' '--no-clobber'
+    abbr -a --command curl -- '--O' '--remote-name-all'
 end
