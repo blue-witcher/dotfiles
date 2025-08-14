7e0bba26a61446483e168b48565802f294edf4de
Blue-Witcher <>
2025-08-14 17:28:19 +0200

    added stow dependency and dependency check for stow and fish

--- fish/install_fish_config.sh
+++ fish/install_fish_config.sh
@@ -1,22 +1,19 @@
 #!/usr/bin/env bash
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-# move existing config.fish and make folders
-mv -i  ~/.config/fish/config.fish  ~/.config/fish/config.fish.backup
-mkdir -p ~/.config/fish/functions
-mkdir -p ~/.config/fish/conf.d
+# check requirements
+if command -v stow >/dev/null 2>&1; then
+    stow -t ~/.config/fish/ --no-folding --ignore='install.*' .
+else
+    echo 'You need to install gnu-stow'
+    exit
+fi
 
-# symlink config.fish, functions/ and conf.d/
-ln -s   "$SCRIPT_DIR"/config.fish   ~/.config/fish/
-## if statement because bash is dumb
-## if there are no *.fish files in the directory, this command makes a symlink to ~/dotfiles/fish/conf.d/*.fish.
-## Because of that, we need to check if there is a *.fish file first and only execute the symlinking if there is
-if ls "$SCRIPT_DIR"/conf.d/*.fish 2> /dev/null; then
-    ln -s "$SCRIPT_DIR"/conf.d/*.fish ~/.config/fish/conf.d/
+if ! command -v fish >/dev/null 2>&1; then
+    echo 'You need to install fish'
+    exit
 fi
-if ls "$SCRIPT_DIR"/functions/*.fish 2> /dev/null; then
-    ln -s "$SCRIPT_DIR"/functions/*.fish ~/.config/fish/functions/
-fi
+
 # set keybinds
 fish -c 'fish_hybrid_key_bindings'
 
