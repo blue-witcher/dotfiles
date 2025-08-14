3235c634595e5fdf18fb61eb259c31953138d8ec
Blue-Witcher <>
2025-08-14 16:41:28 +0200

    cleanup & back to linking files instead of folders

--- fish/install_fish_config.sh
+++ fish/install_fish_config.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-# move existing config.fish, functions/ and conf.d/ out of the way if they exist
-mv -Tn  ~/.config/fish/config.fish  ~/.config/fish/config.fish.backup
-mv -Tn  ~/.config/fish/functions    ~/.config/fish/old_functions
-mv -Tn  ~/.config/fish/conf.d       ~/.config/fish/old_conf.d
+# move existing config.fish and make folders
+mv -i  ~/.config/fish/config.fish  ~/.config/fish/config.fish.backup
+mkdir -p ~/.config/fish/functions
+mkdir -p ~/.config/fish/conf.d
 
 # symlink config.fish, functions/ and conf.d/
 ln -s   "$SCRIPT_DIR"/config.fish   ~/.config/fish/
-ln -sT  "$SCRIPT_DIR"/functions     ~/.config/fish/functions
-ln -sT  "$SCRIPT_DIR"/conf.d        ~/.config/fish/conf.d
-
+## if statement because bash is dumb
+## if there are no *.fish files in the directory, this command makes a symlink to ~/dotfiles/fish/conf.d/*.fish.
+## Because of that, we need to check if there is a *.fish file first and only execute the symlinking if there is
+if ls "$SCRIPT_DIR"/conf.d/*.fish 2> /dev/null; then
+    ln -s "$SCRIPT_DIR"/conf.d/*.fish ~/.config/fish/conf.d/
+fi
+if ls "$SCRIPT_DIR"/functions/*.fish 2> /dev/null; then
+    ln -s "$SCRIPT_DIR"/functions/*.fish ~/.config/fish/functions/
+fi
 # set keybinds
 fish -c 'fish_hybrid_key_bindings'
 
