508b035ef7bd28d823736c35ed234cb4c69c09b9
Blue-Witcher <>
2025-08-17 04:22:45 +0200

    make config dir first

--- fish/restow.sh
+++ fish/restow.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
+mkdir -p ~/.config/fish
 stow -t ~/.config/fish/ --no-folding --ignore='.*\.sh' -d "$SCRIPT_DIR" .
--- helix/stow_helix_config.sh
+++ helix/stow_helix_config.sh
@@ -5,4 +5,5 @@ if ! command -v stow >/dev/null 2>&1; then
     echo 'You need to install gnu-stow'
 fi
 
+mkdir -p ~/.config/helix
 stow -t ~/.config/helix --no-folding --ignore='.*\.sh' -R .
