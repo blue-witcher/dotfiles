c8c4230b2aedb01572eddebe40b77e89476ca004
Blue-Witcher <>
2025-08-17 08:24:26 +0200

    add add back cpp and mvp as rcp and rmv

--- fish/conf.d/rsync.fish
+++ fish/conf.d/rsync.fish
@@ -3,5 +3,7 @@ if status is-interactive
         set -l -- flags --human-readable
         abbr -a rs      "rsync $flags"
         abbr -a rsync   "rsync $flags"
+        abbr -a rcp     "rsync $flags -a --info=progress2"
+        abbr -a rmv     "rsync --remove-source-files $flags -a --info=progress2"
     end
 end
