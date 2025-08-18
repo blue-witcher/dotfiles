712116314b970d893d0e0736cb29c0ee1151a725
Blue-Witcher <>
2025-08-18 10:21:54 +0200

    change permissions and make sure .config/fish exists before stowing

--- fish/install_fish_config.sh
+++ fish/install_fish_config.sh
@@ -3,6 +3,7 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # check requirements
 if command -v stow >/dev/null 2>&1; then
+    mkdir -p ~/.config/fish
     stow -t ~/.config/fish/ --no-folding --ignore='.*\.sh' -d "$SCRIPT_DIR" .
 else
     echo 'You need to install gnu-stow'
