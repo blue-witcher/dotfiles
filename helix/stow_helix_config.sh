9b59b7587ae27ace8b2da717b7ff870f02ffb00c
Blue-Witcher <>
2025-08-18 10:26:50 +0200

    fix helix stow script

--- helix/stow_helix_config.sh
+++ helix/stow_helix_config.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # check requirements
 if ! command -v stow >/dev/null 2>&1; then
@@ -6,4 +7,4 @@ if ! command -v stow >/dev/null 2>&1; then
 fi
 
 mkdir -p ~/.config/helix
-stow -t ~/.config/helix --no-folding --ignore='.*\.sh' -R .
+stow -t ~/.config/helix/ --no-folding --ignore='.*\.sh' -d "$SCRIPT_DIR" .
