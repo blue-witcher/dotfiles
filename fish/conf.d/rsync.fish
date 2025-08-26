50e0f8882984cba69782a42faa022a115a7f4d69
Blue-Witcher <>
2025-08-26 22:08:26 +0200

    made info flag standard for rsync abbrs and changed order in rcp and rmv

--- fish/conf.d/rsync.fish
+++ fish/conf.d/rsync.fish
@@ -1,9 +1,9 @@
 if status is-interactive
     if command -q rsync
-        set -l -- flags --human-readable
+        set -l -- flags --human-readable --info=progress2
         abbr -a rs      "rsync $flags"
         abbr -a rsync   "rsync $flags"
-        abbr -a rcp     "rsync $flags -a --info=progress2"
-        abbr -a rmv     "rsync --remove-source-files $flags -a --info=progress2"
+        abbr -a rcp     "rsync $flags -a"
+        abbr -a rmv     "rsync --remove-source-files $flags -a"
     end
 end
