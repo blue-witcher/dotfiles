e3fbbf7b39b5a64c10ab8e28a6ae64f1529ecfe1
Blue-Witcher <>
2025-09-11 16:40:15 +0200

    both z and y get abbreviated to cd now when $kb_layout isn't set

--- fish/conf.d/cd.fish
+++ fish/conf.d/cd.fish
@@ -1,4 +1,5 @@
 if status is-interactive
+# zoxide
     if command -q zoxide
         zoxide init fish | source
         abbr -a dse                 'zoxide query -i'
@@ -8,12 +9,16 @@ if status is-interactive
                 abbr -a y z
         end
 
+# cd
     else if type -q cd
         switch $kb_layout
             case 'qwertz'
                 abbr -a y cd
+            case 'colemak*' 'qwerty'
+                abbr -a z cd
             case '*'
                 abbr -a z cd
+                abbr -a y cd
         end
     end
 end
