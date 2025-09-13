ab959e42b6b6670edea49e235cbb8f032cdcfd7f
Blue-Witcher <>
2025-09-14 00:35:50 +0200

    made it so the curl opts are a abbreviation _after_ curl (opts) instead of always being abbreviated when typing curl

--- fish/conf.d/curl.fish
+++ fish/conf.d/curl.fish
@@ -1,6 +1,6 @@
 if status is-interactive ;and command -q curl
     set -l -- opts -g -f --proto-default https -L --no-clobber --skip-existing --remove-on-error
-    abbr -a curl "curl $opts"
+    abbr -a --command curl -- 'opts' $opts
     abbr -a --command curl -- '--se' '--skip-existing'
     abbr -a --command curl -- '--nc' '--no-clobber'
     abbr -a --command curl -- '--O' '--remote-name-all'
