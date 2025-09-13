1ace79396fe9303f095552773da9c25ae069ea5b
Blue-Witcher <>
2025-09-14 00:23:33 +0200

    add remove-on-error to basic curl options

--- fish/conf.d/curl.fish
+++ fish/conf.d/curl.fish
@@ -1,5 +1,5 @@
 if status is-interactive ;and command -q curl
-    set -l -- opts -g -f --proto-default https -L --no-clobber --skip-existing
+    set -l -- opts -g -f --proto-default https -L --no-clobber --skip-existing --remove-on-error
     abbr -a curl "curl $opts"
     abbr -a --command curl -- '--se' '--skip-existing'
     abbr -a --command curl -- '--nc' '--no-clobber'
