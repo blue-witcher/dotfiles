57d0c8513e37891f537dc5620b8468d38f3f5a67
Blue-Witcher <>
2025-09-20 02:49:07 +0200

    added header row

--- fish/functions/ll.fish
+++ fish/functions/ll.fish
@@ -1,6 +1,6 @@
 if command -q eza
     function ll --wraps='eza --color=always --group-directories-first --icons --smart-group --git --links --no-quotes --hyperlink -al' --description 'alias ll=ls -al'
-        ls -al $argv
+        ls -alh $argv
     end
 
 else if command -q exa
